ce2e75ba2ca23a2f708970b362a31ddc7fd02ef1
Darkviper7 <m.s.k.tejavarma@gmail.com>
2022-09-05 22:04:05 -0400

    Update add function in main.py

--- code/main.py
+++ code/main.py
@@ -1,21 +1,22 @@
 from distutils import ccompiler
+import math
 
 
 def func(x):
     return x + 1
 
-class Sym:
-    def __init__(self, c = 0, s = ""):
-        self.n = 0
-        self.at = c
-        self.name = s
-        self._has = []
+# class Sym:
+#     def __init__(self, c = 0, s = ""):
+#         self.n = 0
+#         self.at = c
+#         self.name = s
+#         self._has = []
         
     
-    def add(v):
-        if (v != "?"):
-            self.n = self.n + 1
-            self._has[v] = 1 + (self._has[v]) #v or 0
+#     def add(v):
+#         if (v != "?"):
+#             self.n = self.n + 1
+#             self._has[v] = 1 + (self._has[v]) #v or 0
 
 
 
@@ -38,6 +39,20 @@ class Num:
             self._has.sort()
             self.isSorted=True
         return self._has
+
+    def add(v, pos):
+        if v!="?":
+            self.n = self.n + 1
+            self.lo = math.min(v, self.lo)
+            self.hi = math.max(v, self.hi)
+            if len(self._has) < the.nums:
+                pos = 1 + len(self._has)
+            elif math.random() < the.nums/self.n:
+                pos = math.random(len(self._has))
+            if pos:
+                self.isSorted = False
+                self._has[pos] = int(v)
+
 
     def div(self):
         return self.x  # standard deviation - sort numbers, find 90th, 10th percentile, return (90th-10th)/2.56
